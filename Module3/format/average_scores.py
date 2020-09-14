7c53ab57891ef902ef310d3586b3981010b7a6ba
puckdisc <sean.k.gilbert@gmail.com>
2020-09-13 22:11:03 -0500

    Update average_scores.py
    names and output added successfully

--- Module3/format/average_scores.py
+++ Module3/format/average_scores.py
@@ -1,9 +1,21 @@
+# Sean Gilbert
+# Last Modified 09/13/20
+# collects inputs, calculates average, outputs name and scores
+
 def average():
     score1 = int(input("Enter the first score: "))
     score2 = int(input("Enter the second score: "))
     score3 = int(input("Enter the third score: "))
-    return (score1+score2+score3)/3
+    avg = (score1+score2+score3)/3
+
+    return round(avg, 2)
+
 
 if __name__ == '__main__':
-    average()
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    age = int(input("Enter your age: "))
 
+    average_scores = average()
+
+    print(last_name + ',', first_name, ' Age:', age, ' Grade:', average_scores)
