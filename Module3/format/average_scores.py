50de35cdcf415546949cb269f93d17a038e4ad8a
puckdisc <sean.k.gilbert@gmail.com>
2020-09-13 21:55:51 -0500

    Module 3
    test avg ok

--- Module3/format/average_scores.py
+++ Module3/format/average_scores.py
@@ -1,2 +1,9 @@
 def average():
-    pass
+    score1 = int(input("Enter the first score: "))
+    score2 = int(input("Enter the second score: "))
+    score3 = int(input("Enter the third score: "))
+    return (score1+score2+score3)/3
+
+if __name__ == '__main__':
+    average()
+
