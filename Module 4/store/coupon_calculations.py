44f5082135795d5b48b74567972de14ce15a8079
puckdisc <sean.k.gilbert@gmail.com>
2020-09-21 22:26:00 -0500

    Topic 3: 1st set passing

--- Module 4/store/coupon_calculations.py
+++ Module 4/store/coupon_calculations.py
@@ -1,7 +1,22 @@
-def calculate_price(price, cash_coupon, percent_coupon):
+def calculate_order(price, cash_coupon, percent_coupon):
+    tax = 0.06
+    subtotal = (price - cash_coupon)*(1-percent_coupon)
 
-    pass
+    if subtotal < 0:
+        subtotal  = 0
+    if 0 <= price < 10:
+        shipping = 5.95
+    elif 10 <= price < 30:
+        shipping = 7.95
+    elif 30 <= price < 50:
+        shipping = 11.95
+    elif 50 <= price:
+        shipping = 0
+
+
+    return subtotal*(1+tax)+shipping
 
 
 if __name__ == '__main__':
+
     calculate_price()
--- Module 4/test/test_coupon_calculations.py
+++ Module 4/test/test_coupon_calculations.py
@@ -4,12 +4,12 @@ from store import coupon_calculations
 class MyTestCase(unittest.TestCase):
     def test_price_under_ten(self):
         price = 8
-        self.assertAlmostEqual(8.65,coupon_calculations.calculate_price(price, 5, 0.1),3)
-        self.assertAlmostEqual(8.5,coupon_calculations.calculate_price(price, 5, 0.15),3)
-        self.assertAlmostEqual(8.35,coupon_calculations.calculate_price(price, 5, 0.2),3)
-        self.assertAlmostEqual(0,coupon_calculations.calculate_price(price, 10, 0.1),3)
-        self.assertAlmostEqual(0,coupon_calculations.calculate_price(price, 10, 0.15),3)
-        self.assertAlmostEqual(0,coupon_calculations.calculate_price(price, 10, 0.2),3)
+        self.assertAlmostEqual(8.81,coupon_calculations.calculate_order(price, 5, 0.1),2)
+        self.assertAlmostEqual(8.65,coupon_calculations.calculate_order(price, 5, 0.15),2)
+        self.assertAlmostEqual(8.49,coupon_calculations.calculate_order(price, 5, 0.2),2)
+        self.assertAlmostEqual(5.95,coupon_calculations.calculate_order(price, 10, 0.1),2)
+        self.assertAlmostEqual(5.95,coupon_calculations.calculate_order(price, 10, 0.15),2)
+        self.assertAlmostEqual(5.95,coupon_calculations.calculate_order(price, 10, 0.2),2)
 
 
 if __name__ == '__main__':
