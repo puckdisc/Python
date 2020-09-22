f71aac559e7d03ffd24b463bfd6c5ccdefcf698c
puckdisc <sean.k.gilbert@gmail.com>
2020-09-21 23:37:43 -0500

    Topic 4: Raises ValueError if only score2 negative

--- Module 4/input_validation/validation_with_try.py
+++ Module 4/input_validation/validation_with_try.py
@@ -7,7 +7,7 @@
 def average(score1, score2, score3):
     NUMBER_TESTS = 3
 
-    if score1 < 0:
+    if score2 < 0:
             raise ValueError
 
     return float((score1 + score2 + score3)/NUMBER_TESTS)
--- Module 4/test/test_validation_with_try.py
+++ Module 4/test/test_validation_with_try.py
@@ -9,7 +9,7 @@ from input_validation import validation_with_try
 class MyTestCase(unittest.TestCase):
     def test_average_negative_input(self):
         with self.assertRaises(ValueError):
-            validation_with_try.average(-90, 89, 78)
+            validation_with_try.average(90, -89, 78)
 
 
 
