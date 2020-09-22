df8bac9c337d824a4ede93df9c1f2238e7174159
puckdisc <sean.k.gilbert@gmail.com>
2020-09-21 23:42:18 -0500

    Topic 4: only raises value error if score2 neg

--- Module 4/test/test_validation_with_try.py
+++ Module 4/test/test_validation_with_try.py
@@ -9,6 +9,9 @@ from input_validation import validation_with_try
 class MyTestCase(unittest.TestCase):
     def test_average_negative_input(self):
         with self.assertRaises(ValueError):
+            validation_with_try.average(-90, 89, 78)
+
+        with self.assertRaises(ValueError):
             validation_with_try.average(90, -89, 78)
 
 
