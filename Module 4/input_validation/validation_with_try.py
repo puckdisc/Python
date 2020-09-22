7c76e77770264a20d6c70831740b28bafefb50ad
puckdisc <sean.k.gilbert@gmail.com>
2020-09-21 23:24:23 -0500

    Topic 4: No ValueError raised

---
+++ Module 4/input_validation/validation_with_try.py
@@ -0,0 +1,14 @@
+# Sean Gilbert
+# CIS 189 Fall 2020
+# Last Modified 09/21/20
+
+
+
+def average(score1, score2, score3):
+    NUMBER_TESTS = 3
+
+    return float((score1 + score2 + score3)/NUMBER_TESTS)
+
+
+if __name__ == '__main__':
+    average()
---
+++ Module 4/test/test_validation_with_try.py
@@ -0,0 +1,17 @@
+# Sean Gilbert
+# CIS 189 Fall 2020
+# unit test input validation
+
+import unittest
+from input_validation import validation_with_try
+
+
+class MyTestCase(unittest.TestCase):
+    def test_average_negative_input(self):
+        with self.assertRaises(ValueError):
+            validation_with_try.average(-90, 89, 78)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
