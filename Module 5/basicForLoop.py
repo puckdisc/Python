ba6fde256d8e34d8450712b2eefd259931fd4311
puckdisc <sean.k.gilbert@gmail.com>
2020-09-27 20:33:43 -0500

    Basic For Loops Assignment

---
+++ Module 5/basicForLoop.py
@@ -0,0 +1,12 @@
+# Sean Gilbert
+# CIS 189 Fall 2020
+# Module 5 Topic 1: Basic for Loops Assignment
+# Demonstrate use of for loops
+
+list_sean = [1.2, 2.2, 3.8, 4, 12, 11.3]
+
+for y in list_sean:
+    print(y)
+
+for x in range(101,31,-2):
+    print(x)
