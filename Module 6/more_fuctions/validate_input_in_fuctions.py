ebc7d8e1d346fcece679d5509497f46cefd58f67
puckdisc <sean.k.gilbert@gmail.com>
2020-10-05 20:30:19 -0500

    Module 6 Topic 4: Function Default Values Assignment

--- Module 6/more_fuctions/validate_input_in_fuctions.py
+++ Module 6/more_fuctions/validate_input_in_fuctions.py
@@ -14,13 +14,6 @@ def score_input(test_name, test_score=0, invalid_message='Invalid test score, tr
         if test_name == '':
             return False
 
-        if 0 <= test_score <= 100:
-            print(test_name, test_score)
-            # return {test_name: test_score}
-            return True
-        else:
-            print(invalid_message)
-            return False
 
 
 if __name__ == '__main__':
