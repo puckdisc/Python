21b9383b7086f7eefddaf5321fd4fe69e2e98d1e
puckdisc <sean.k.gilbert@gmail.com>
2020-10-05 20:28:02 -0500

    Module 6 Topic 4: Function Default Values Assignment

---
+++ Module 6/more_fuctions/validate_input_in_fuctions.py
@@ -0,0 +1,15 @@
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
+    """prompt for test name and test score then prints the inputs
+
+    The test name string must not be empty. The test score must be between
+    0 and 100. If both inputs are valid, they are printed.
+
+    :param test_name: string name of test
+    :param test_score: integer test score
+    :param invalid_message: string error message
+    :return: pass
+    """
+    pass
+
+if __name__ == '__main__':
+    score_input()
