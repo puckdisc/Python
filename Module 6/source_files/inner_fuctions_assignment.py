f5b0503a115a8bded402f9b22277d79151f8bd00
puckdisc <sean.k.gilbert@gmail.com>
2020-10-05 22:30:59 -0500

    Module 6 Topic 5: Inner Function Assignment

--- Module 6/source_files/inner_fuctions_assignment.py
+++ Module 6/source_files/inner_fuctions_assignment.py
@@ -1,10 +1,16 @@
 def measurements(measurement_list):
     """
+    calculates area and perimeter of rectangles/squares
+
+    function first determines if only one value is present in measurement_list
+
 
 
     :param measurement_list: numeric dimensions of shape
     :return: string describing results
     """
+    if len(measurement_list) == 1:
+        measurement_list.append(measurement_list[0])
 
     def area(measurement_list):
         return float(measurement_list[0]*measurement_list[1])
