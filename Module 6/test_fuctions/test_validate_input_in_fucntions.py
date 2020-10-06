c0b9fca4b074fef25b0fd8b085b4e96af8d45256
puckdisc <sean.k.gilbert@gmail.com>
2020-10-05 20:26:55 -0500

    Module 6 Topic 4: Function Default Values Assignment

---
+++ Module 6/test_fuctions/test_validate_input_in_fucntions.py
@@ -0,0 +1,26 @@
+import unittest
+from more_fuctions import validate_input_in_fuctions
+
+
+class MyTestCase(unittest.TestCase):
+    def test_score_input_test_name(self):
+        self.assertEqual(False, validate_input_in_fuctions.score_input(''))
+
+    def test_score_input_test_score_valid(self):
+        self.assertEqual(True, validate_input_in_fuctions.score_input('abc', 50))
+
+    def test_score_input_test_score_below_range(self):
+        self.assertEqual(False, validate_input_in_fuctions.score_input('abc', -10))
+
+    def test_score_input_test_score_above_range(self):
+        self.assertEqual(False, validate_input_in_fuctions.score_input('abc', 105))
+
+    def test_score_input_test_score_non_numeric(self):
+        self.assertEqual(False, validate_input_in_fuctions.score_input('abc', 'abc'))
+
+    def test_score_input_invalid_message(self):
+        self.assertEqual(True, validate_input_in_fuctions.score_input('abc',500), 'passed into argument')
+
+
+if __name__ == '__main__':
+    unittest.main()
