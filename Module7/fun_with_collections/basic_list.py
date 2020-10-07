665496072a8ab8f24c3bb34ece3b4f4fa3c18b84
puckdisc <sean.k.gilbert@gmail.com>
2020-10-06 21:44:42 -0500

    Module 7 Topic 1: Lists

--- Module7/fun_with_collections/basic_list.py
+++ Module7/fun_with_collections/basic_list.py
@@ -1,7 +1,21 @@
 def make_list():
-    pass
+    fun_list = []
+
+    try:
+        for x in range(3):
+            var = [get_input()]
+            fun_list[len(fun_list):] = var
+            fun_list[x] = int(fun_list[x])
+
+    except ValueError:
+        print('Non-integer input detected!!!1!11!')
+
+    print(fun_list)
+    return fun_list
 
 
 def get_input():
     pass
 
+if __name__ == '__main__':
+    make_list()
