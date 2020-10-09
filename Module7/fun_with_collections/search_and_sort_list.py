e4d8fc26c35d446d11d42b996bc9a7508ad1a3bb
puckdisc <sean.k.gilbert@gmail.com>
2020-10-08 20:14:35 -0500

    Module 7 Topic 1 List Search and Sort List Assignment

--- Module7/fun_with_collections/search_and_sort_list.py
+++ Module7/fun_with_collections/search_and_sort_list.py
@@ -1,25 +1,42 @@
 from fun_with_collections import basic_list_exception
 
+
 def sort_list():
-    """
 
-    :return:
     """
+    Sorts list
 
+    Calls basic_list_exception.make_list(). The return is assigned variable name
+    fun_list. Fun_list is sorted and returned.
+
+    :return: fun_list: sorted list
+    """
     fun_list = basic_list_exception.make_list()
     fun_list.sort()
     return fun_list
 
 
-def search_list():
+def search_list(search):
     """
+    Returns the index of the desired list item
 
-    :return:
+    Calls basic_list_exception.make_list(). This return is assigned variable name
+    fun_list. Parameter 'search' is searched for in fun_list and the index returned.
+    An invalid search will return -1. This function will only return the first occurrence
+    of the desired item. Multiple occurrences are not accounted for.
+
+
+    :param search: integer to search for
+    :return: location: integer index of desired search in fun_list
     """
-    return 1
-
-
+    fun_list = basic_list_exception.make_list()
+    for x in range(len(fun_list)):
+        try:
+            location = fun_list.index(search)
+            return location
+        except ValueError:
+            return -1
 
 
 if __name__ == '__main__':
-    sort_list()
+    search_list(2)
--- Module7/test_collections/test_search_and_sort_list.py
+++ Module7/test_collections/test_search_and_sort_list.py
@@ -10,8 +10,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(search_and_sort_list.sort_list(), [9, 11, 45])
 
     @patch('fun_with_collections.basic_list_exception.make_list', return_value=[11, 9, 45])
-    def test_search_list(self, input):
-        self.assertEqual(search_and_sort_list.search_list(), 1)
+    def test_search_list_valid_single(self, input):
+        self.assertEqual(search_and_sort_list.search_list(9), 1)
+
+    @patch('fun_with_collections.basic_list_exception.make_list', return_value=[11, 9, 45])
+    def test_search_list_invalid(self, input):
+        self.assertEqual(search_and_sort_list.search_list(16), -1)
 
 if __name__ == '__main__':
     unittest.main()
