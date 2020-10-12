a2690057605e52de7ba1443c5b361e88d5e77f88
puckdisc <sean.k.gilbert@gmail.com>
2020-10-11 22:33:09 -0500

    Module 7 Topic 4 Search and Sort Array

---
+++ Module7/fun_with_collections/sort_and_search_array.py
@@ -0,0 +1,12 @@
+from fun_with_collections import basic_list_exception
+
+def search_array(search):
+    pass
+
+
+def sort_array():
+    pass
+
+
+if __name__ == '__main__':
+     basic_list_exception.make_list()
