e23765c78172bafc5a7683850683f8e1ebffda74
puckdisc <sean.k.gilbert@gmail.com>
2020-10-11 23:09:24 -0500

    Module 7 Topic 4 Search and Sort Array

--- Module7/fun_with_collections/sort_and_search_array.py
+++ Module7/fun_with_collections/sort_and_search_array.py
@@ -1,7 +1,29 @@
+from array import array as arr
 from fun_with_collections import basic_list_exception
 
+
 def search_array(search):
-    pass
+
+    """
+    Searches array for value and returns index
+
+    Calls basic_list.make_list() and accepts list return. Converts list to
+    signed integer array. Takes param search and returns index or -1
+    if not found.
+
+
+    :param search: integer to search for
+    :return: search_index: integer index of search
+    """
+
+    fun_list = basic_list_exception.make_list()
+    fun_array = arr('i', fun_list)
+    try:
+        search_index = fun_array.index(search)
+        print(str(search) + ' is at ' + str(search_index))
+        return search_index
+    except ValueError:
+        return -1
 
 
 def sort_array():
@@ -9,4 +31,4 @@ def sort_array():
 
 
 if __name__ == '__main__':
-     basic_list_exception.make_list()
+    search_array(6)
--- Module7/test_collections/test_sort_and_search_array.py
+++ Module7/test_collections/test_sort_and_search_array.py
@@ -6,11 +6,11 @@ from fun_with_collections import sort_and_search_array
 class MyTestCase(unittest.TestCase):
 
     @patch('fun_with_collections.basic_list_exception.make_list', return_value=[11, 9, 45])
-    def test_search_list_valid_single(self, input):
+    def test_search_array_valid_single(self, input):
         self.assertEqual(sort_and_search_array.search_array(9), 1)
 
     @patch('fun_with_collections.basic_list_exception.make_list', return_value=[11, 9, 45])
-    def test_search_list_invalid(self, input):
+    def test_search_array_invalid(self, input):
         self.assertEqual(sort_and_search_array.search_array(16), -1)
 
 if __name__ == '__main__':
