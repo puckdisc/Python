168451acf601dc9b0678250df8489f2d180d51fc
puckdisc <sean.k.gilbert@gmail.com>
2020-10-15 19:58:52 -0500

    Module 8 Topic 1 Sets

---
+++ Module8/more_fun_with_collections/set_membership.py
@@ -0,0 +1,8 @@
+def in_set(fun_set, fun_set_check):
+    pass
+
+
+
+
+if __name__ =='__main__':
+    in_set()
---
+++ Module8/test/test_set_membership.py
@@ -0,0 +1,16 @@
+import unittest
+from more_fun_with_collections import set_membership
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_in_set_true(self):
+        self.assertEqual(True, set_membership.in_set({1, 2, 3, 4, 5, 6}, {5}))
+
+    def test_in_set_false(self):
+        self.assertEqual(False, set_membership.in_set({1, 2, 3, 4, 5, 6}, {9}))
+
+
+if __name__ == '__main__':
+
+    unittest.main()
