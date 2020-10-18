ee52f869d60170e29709c416c645cf75fa0be82f
puckdisc <sean.k.gilbert@gmail.com>
2020-10-18 13:18:01 -0500

    Module 8 Topic 2 Dictionaries

--- Module8/more_fun_with_collections/dict_membership.py
+++ Module8/more_fun_with_collections/dict_membership.py
@@ -1,8 +1,12 @@
 def in_dict(fun_dict, search):
-    pass
+    try:
+        return search in fun_dict
+    except:
+        return False
 
 
 
 
 if __name__ == '__main__':
-    pass
+    d = {'A': 90, 'B': 80, 'C': 70, 'D': 60, 'F': 0}
+    print(in_dict(d, 'A'))
