1ba6def81d516b9f000da873454906a32621b3aa
puckdisc <sean.k.gilbert@gmail.com>
2020-10-18 19:39:21 -0500

    Module 8 Topic 4 DateTime

--- Module8/more_fun_with_collections/half_birthday.py
+++ Module8/more_fun_with_collections/half_birthday.py
@@ -1,7 +1,13 @@
-from datetime import datetime, timedelta
+import datetime
+from dateutil.relativedelta import relativedelta
+
 
 def half_birthday(birthday):
-    pass
+    half_birthday = birthday + relativedelta(months=+6)
+    return half_birthday
+
+
 
 if __name__ == '__main__':
-    pass
+    birthday = datetime.datetime(2015, 5, 10)
+    half_birthday(birthday)
