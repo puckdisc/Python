896270be6a97c6a1010f8c00279e444d2bfc4f2f
puckdisc <sean.k.gilbert@gmail.com>
2020-11-01 21:04:04 -0600

    Module 10 Topic 3 Testing Classes

--- Module10/class_definitions/student.py
+++ Module10/class_definitions/student.py
@@ -1,8 +1,9 @@
 class Student:
     """Student class"""
     def __init__(self, lname, fname, major, gpa=0.0):
-        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
-        if not name_characters.issuperset(lname) or not name_characters.issuperset(fname):
+        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'- ")
+        if not name_characters.issuperset(lname) or not name_characters.issuperset(fname) \
+                or not name_characters.issuperset(major):
             raise ValueError
         self.last_name = lname
         self.first_name = fname
@@ -18,6 +19,7 @@ class Student:
     def display(self):
         return self.first_name + ' ' + self.last_name + " is majoring in " + self.major +\
                                      " with GPA: " + str(self.gpa)
+
 
 # Driver
 student = Student('Ovechkin', 'Alex', 'Goal Scoring', 4.0)
