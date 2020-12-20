c461f3574d0f62c4879e1413e5124d40425dfa67
puckdisc <sean.k.gilbert@gmail.com>
2020-12-20 00:33:41 -0600

    initial commit
    initial commit

---
+++ FleaFlicker/league.py
@@ -0,0 +1,37 @@
+import requests
+
+
+def get_team_ids(league):
+
+    """
+    :param league: league_id
+    :return: returns a list of team_ids in the league
+    """
+
+
+    params = {}
+    params['sport'] = 'NHL'
+    params['league_id'] = league
+    params['season'] = 2019
+
+    url = 'https://www.fleaflicker.com/api/FetchLeagueRosters'
+
+    r = requests.get(url, params)  #Fetch LeaguePlayerListing
+
+    r_json = r.json()  # json
+
+    LeagueRoster = r_json['rosters']
+
+    team_ids = []
+    for a in LeagueRoster:
+        b = a['team']
+        team_ids.append(b['id'])  # pull just team id, append to return list
+
+    r.close()
+    del r
+    return team_ids
+
+
+
+if __name__ == "__main__":
+    get_team_ids(12093)
---
+++ FleaFlicker/main.py
@@ -0,0 +1,14 @@
+import draft
+import players
+import league
+
+
+
+
+if __name__ == "__main__":
+
+    leagues = []
+    for x in range(12086, 12102):  # first and last league num for OTH 2019
+        leagues.append(x)
+
+    print(leagues)
